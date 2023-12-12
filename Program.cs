10ea25cf6333e3d77bfe9c7af31ddbfd8036ee39
00xd1 <00xd1.dev@gmail.com>
2023-12-12 10:40:35 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+Guid myuuid = Guid.NewGuid();
+string uuid = myuuid.ToString();
+
+
+string firstName;
+string lastName;
+int age;
+firstName = "henrik";
+lastName = "alderman";
+age = 56;
+Console.Write(firstName + " " + lastName + " " + age + " " + uuid);
